0d658722f639d543ba6b990de5334d996d35a0fe
FacuA0 <facundoamarilla28@gmail.com>
2022-08-18 17:12:16 -0300

    Made the intro to appear exactly when loaded

--- src/js/intro.js
+++ src/js/intro.js
@@ -1,7 +1,16 @@
 let intro = document.querySelector(".intro");
 if (Cookie.get("intro")!=""){
-	document.body.removeChild(intro);
+	intro.parentElement.removeChild(intro);
 }
 else {
 	Cookie.set("intro", 1, 20);
 }
+
+// Start intro animations when fully loaded.
+window.addEventListener("load", () => {
+	document.querySelector(".intro").style.animationName = "make-visible";
+
+	document.querySelectorAll(".intro-letter").forEach((letter => {
+		letter.style.animationName = "outer-animate";
+	}));
+});
